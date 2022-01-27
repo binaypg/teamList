931c63c59af137851bd4bb657e3f24a066cc167c
Binay kumar Sharma <binay.pg@gmail.com>
2022-01-27 19:32:17 +0530

    Initial Commit

---
+++ Team/TeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamApp.swift
+//  Team
+//
+//  Created by Binay Kumar Sharma on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
